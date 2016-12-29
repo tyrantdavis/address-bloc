19a620717419703aa799c7b7926161964f865205
tyrantdavis <davistyrant@gmail.com>
2016-12-29 11:39:07 -0600

    Complete assignment-address-bloc-searches

--- models/address_book.rb
+++ models/address_book.rb
@@ -54,4 +54,13 @@ class AddressBook
     # #5
     return nil
   end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil
+  end
 end
