f110b9848cd38d2e1e6aaf88cc4ed2e7b15189e0
tyrantdavis <davistyrant@gmail.com>
2016-12-28 18:25:09 -0600

    Complete assignment-address-bloc-reading-csvs

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -61,20 +61,19 @@ RSpec.describe AddressBook do
         check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
       end
 
-    it "imports the 2nd entry" do
-      book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/models/entries.csv")
-      # Check the second entry
-      entry_two = book.entries[1]
-      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
-    end
+      it "imports the 2nd entry" do
+        book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/models/entries.csv")
+        # Check the second entry
+        entry_two = book.entries[1]
+        check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+      end
 
-    it "imports the 3rd entry" do
-      book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/models/entries.csv")
-      # Check the third entry
-      entry_three = book.entries[2]
-      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
-
-    end
+      it "imports the 3rd entry" do
+        book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/models/entries.csv")
+        # Check the third entry
+        entry_three = book.entries[2]
+        check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+      end
 
     it "imports the 4th entry" do
       book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/models/entries.csv")
@@ -89,6 +88,36 @@ RSpec.describe AddressBook do
       # Check the fifth entry
       entry_five = book.entries[4]
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+    end
+  end
+
+  context "importing from /Users/JupiterCody/bloc/address-bloc/spec/entries_2.csv" do
+    it "imports the correct number of entries"do
+      book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/spec/entries_2.csv")
+
+      expect(book.entries.size).to eq(3)
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/spec/entries_2.csv")
+      # Check the first entry
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/spec/entries_2.csv")
+      # Check the second entry
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("/Users/JupiterCody/bloc/address-bloc/spec/entries_2.csv")
+      # Check the third entry
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+
     end
   end
 end
